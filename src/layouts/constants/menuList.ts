9af49952ab9938a1e5f796f3eb6265058dfe7de1
Mechi-Warnes <mercewarnes@gmail.com>
2025-01-29 23:48:34 -0300

    fix: cambio de icono

--- src/layouts/constants/menuList.ts
+++ src/layouts/constants/menuList.ts
@@ -1,11 +1,11 @@
-import { AutoStories, Logout, Home, Group } from '@mui/icons-material'
+import { AutoStories, Logout, Home, Group, MenuBook } from '@mui/icons-material'
 import { MenuItem, MenuList } from '../types/MenuList'
 
 export const menuList: MenuList = {
   top: [
     { label: 'Home', path: '/', Icon: Home, activeOnly: true },
     { label: 'Miembros', path: '/miembros', Icon: Group, adminOnly: true, activeOnly: true },
-    { label: 'Books', path: '/books', Icon: AutoStories, activeOnly: true },
+    { label: 'Books', path: '/books', Icon: MenuBook, activeOnly: true },
   ],
   bottom: [
     { label: 'Log Out', path: '/logout', Icon: Logout },
