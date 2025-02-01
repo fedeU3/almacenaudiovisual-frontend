1ba93264696e5f62f431f1a7b88442ee126c8d08
Mechi-Warnes <mercewarnes@gmail.com>
2025-02-01 13:19:27 -0300

    fix: mejora de calidad de los logos, ajuste de diseño de Login y SignUP

--- src/layouts/components/AppBar.tsx
+++ src/layouts/components/AppBar.tsx
@@ -1,42 +1,54 @@
-import React from 'react'
-import { AppBar, Box, Toolbar, Typography, Button, useTheme } from '@mui/material'
+import React from 'react';
+import { AppBar, Box, Toolbar, Typography, Button, useTheme } from '@mui/material';
 
 type NavBarProps = {
-  goTo: (path: string) => () => void
-  currentPage: string
-}
+  goTo: (path: string) => () => void;
+  currentPage: string;
+};
 
-const NavBar: React.FC<NavBarProps> = ({
-  goTo,
-  currentPage,
-}) => {
+const NavBar: React.FC<NavBarProps> = ({ goTo, currentPage }) => {
   const theme = useTheme();
   return (
     <Box sx={{ flexGrow: 1 }}>
-      <AppBar position="fixed" sx={{ backgroundColor: "#2C3E50" }}>
-        <Toolbar sx={{ justifyContent: "space-between" }}>
+      <AppBar position="fixed" sx={{ backgroundColor: '#2C3E50' }}>
+        <Toolbar sx={{ justifyContent: 'space-between' }}>
+          {/* Contenedor para el logo y el texto */}
           <Box display="flex" alignItems="center">
-            <Typography variant="h6" component="div" sx={{ flexGrow: 1 }}>
+            {/* Logo */}
+            <Box component="img" src="/LogoFondo.svg" alt="Logo Almacén" sx={{ height: 40, mr: 2 }} />
+            {/* Texto de currentPage */}
+            <Typography variant="h6" component="div">
               {currentPage}
             </Typography>
-            <Box component="img" src="/LogoFondo.svg" alt="Logo Almacén" sx={{ height: 40, mr: 2 }} />
           </Box>
+
+          {/* Botones a la derecha */}
           <Box>
-            <Button variant="contained" sx={{ backgroundColor: "#B0BEC5", color: "#000", border: '1px solid #000', mr: 1 }}>
+            <Button
+              variant="contained"
+              sx={{ backgroundColor: '#B0BEC5', color: '#000', border: '1px solid #000', mr: 1 }}
+            >
               Alquilar Equipo
             </Button>
-            <Button onClick={goTo('/login')} color="inherit" sx={{ backgroundColor: "#FF7043", border: '1px solid #080808', color: "#080808" }}>
+            <Button
+              onClick={goTo('/login')}
+              color="inherit"
+              sx={{ backgroundColor: '#FF7043', border: '1px solid #080808', color: '#080808' }}
+            >
               Login
             </Button>
-            <Button onClick={goTo('/signup')} variant="outlined" sx={{ ml: 1, backgroundColor: "#080808", borderColor: "#FF7043", color: "#FF7043", border: '1px solid #FF7043' }}>
+            <Button
+              onClick={goTo('/signup')}
+              variant="outlined"
+              sx={{ ml: 1, backgroundColor: '#080808', borderColor: '#FF7043', color: '#FF7043', border: '1px solid #FF7043' }}
+            >
               Sign up
             </Button>
           </Box>
         </Toolbar>
       </AppBar>
     </Box>
-  )
-}
+  );
+};
 
-export default NavBar
-
+export default NavBar;
