c06d07a3303df94a86afe8dea2d515c07402a748
Mechi-Warnes <mercewarnes@gmail.com>
2025-03-10 03:26:43 -0300

    Revert "feat: Añade componente Footer y lo integra en el layout de la aplicación"
    This reverts commit 58dde0e7c32f96c52ca2bd549e6baa0b268625c6.

--- src/App.tsx
+++ src/App.tsx
@@ -9,25 +9,23 @@ import Users from './pages/Usuarios';
 import Logout from './pages/Logout';
 import MyOrders  from './pages/MisPedidos';
 import CreateOrder from './pages/CrearPedidos';
-import AppLayout from './layouts/app/AppLayout'; // Importa AppLayout
 
 function App() {
   return (
-    <Routes>
-      <Route path={ROUTES.login.path} element={<Login/>} />
-      <Route path={ROUTES.logout.path} element={<Logout/>} />
-      {/* Envuelve las demás rutas con AppLayout */}
-      <Route element={<AppLayout currentPage="Home" children={undefined} />}>
-        <Route path={ROUTES.home.path} element={<Home />} />
-        <Route path={ROUTES.signup.path} element={<SignUp />} />
-        <Route path={ROUTES.books.path} element={<Books />} />
-        <Route path={ROUTES.usuarios.path} element={<Users />} />
-        <Route path={ROUTES.miembros.path} element={<Miembros />} />
-        <Route path={ROUTES.MisPedidos.path} element={<MyOrders />} />
-        <Route path={ROUTES.createOrders.path} element={<CreateOrder />} />
-      </Route>
-    </Routes>
+    <>
+      <Routes>
+        <Route path={ROUTES.home.path} element={<Home/>} />
+        <Route path={ROUTES.login.path} element={<Login/>} />
+        <Route path={ROUTES.signup.path} element={<SignUp/>} />
+        <Route path={ROUTES.books.path} element={<Books/>} />
+        <Route path={ROUTES.logout.path} element={<Logout/>} />
+        <Route path={ROUTES.usuarios.path} element={<Users/>} />
+        <Route path={ROUTES.miembros.path} element={<Miembros/>} />
+        <Route path={ROUTES.MisPedidos.path} element={<MyOrders/>} />
+        <Route path={ROUTES.createOrders.path} element={<CreateOrder/>} />
+      </Routes>
+    </>
   )
 }
 
-export default App;
+export default App
--- src/pages/Home/index.tsx
+++ src/pages/Home/index.tsx
@@ -2,6 +2,14 @@ import React from 'react';
 import { Button, Container, Grid, Card, CardContent, CardMedia, Typography, IconButton, Box } from "@mui/material";
 import ArrowForwardIcon from '@mui/icons-material/ArrowForward';
 import InventoryIcon from '@mui/icons-material/Inventory';
+import XIcon from '@mui/icons-material/X';
+import InstagramIcon from '@mui/icons-material/Instagram';
+import YouTubeIcon from '@mui/icons-material/YouTube';
+import LinkedInIcon from '@mui/icons-material/LinkedIn';
+import HomeIcon from '@mui/icons-material/Home';
+import PhoneIcon from '@mui/icons-material/Phone';
+import EmailIcon from '@mui/icons-material/Email';
+import AccessTimeIcon from '@mui/icons-material/AccessTime';
 
 export default function Home() {
   return (
@@ -65,6 +73,44 @@ export default function Home() {
           ))}
         </Grid>
       </Container>
+
+      {/* Footer */}
+      <Box sx={{ backgroundColor: "#2C3E50", textAlign: "center", py: 4, mt: 4 }}>
+        <Grid container spacing={2} justifyContent="center">
+          <Grid item xs={12} sm={4}>
+            <Box component="img" src="/LogoChocho.svg" alt="Producciones" sx={{ height: 110 }} />
+            <Box display="flex" justifyContent="center" sx={{ mt: 2 }}>
+              <IconButton color="inherit"><XIcon /></IconButton>
+              <IconButton color="inherit"><InstagramIcon /></IconButton>
+              <IconButton color="inherit"><YouTubeIcon /></IconButton>
+              <IconButton color="inherit"><LinkedInIcon /></IconButton>
+            </Box>
+          </Grid>
+          <Grid item xs={12} sm={4}>
+            <Typography variant="h6">Alquiler de equipo audiovisual</Typography>
+            <Box display="flex" alignItems="center" sx={{ mt: 2 }}>
+              <HomeIcon />
+              <Typography variant="body2" sx={{ ml: 1 }}>Calle Falsa 123</Typography>
+            </Box>
+            <Box display="flex" alignItems="center" sx={{ mt: 1 }}>
+              <PhoneIcon />
+              <Typography variant="body2" sx={{ ml: 1 }}>123-456-7890</Typography>
+            </Box>
+            <Box display="flex" alignItems="center" sx={{ mt: 1 }}>
+              <EmailIcon />
+              <Typography variant="body2" sx={{ ml: 1 }}>correo@almacen.com</Typography>
+            </Box>
+            <Box display="flex" alignItems="center" sx={{ mt: 1 }}>
+              <AccessTimeIcon />
+              <Typography variant="body2" sx={{ ml: 1 }}>L a V de 9:00 AM a 6:00 PM</Typography>
+            </Box>
+          </Grid>
+          <Grid item xs={12} sm={4}>
+            <Box component="img" src="/LogoMonocromatico.svg" alt="Logo" sx={{ height: 120, mb: 1 }} />
+            <Typography variant="body2">Almacén Audiovisual</Typography>
+          </Grid>
+        </Grid>
+      </Box>
     </Box>
   );
 }
